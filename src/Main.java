82da9073f2e855ab1d53f619635e063c7a9a9396
martin981022 <136131290+martin981022@users.noreply.github.com>
2023-06-09 18:25:43 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int param1 = 10;
+        int param2 = 30;
+        int param3 = 40;
+
+        suma(param1 + param2 + param3);
+
+
+        System.out.println(a);
+    }
+
+public static void suma(int a, int b)[
+        System.out.println(a + b);
+]
+    }
+
