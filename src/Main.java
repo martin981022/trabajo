79472e30369dd85f6659d228a46bea20113c5244
martin981022 <136131290+martin981022@users.noreply.github.com>
2023-06-09 18:40:31 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         int param1 = 10;
         int param2 = 30;
         int param3 = 40;
 
-        suma(param1 + param2 + param3);
+        var suma = (param1 + param2 + param3);
 
 
-        System.out.println(a);
+        System.out.println(suma);
     }
-
-public static void suma(int a, int b)[
-        System.out.println(a + b);
-]
-    }
+}
 
